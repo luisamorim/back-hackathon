fbc800f5e64900671245f8cd0d936f7fd8be4929
luisamorim <kicoamorim@gmail.com>
2020-12-17 15:22:41 -0300

    feat: add socket.io

--- index.js
+++ index.js
@@ -1,8 +1,53 @@
 require('dotenv').config();
+
 const express = require('express');
 const app = express();
+const http = require('http').createServer(app);
 const mongo = require('./mongo');
 const bodyParser = require('body-parser')
+
+const io = require('socket.io')(http, {
+    cors: {
+      origin: '*',
+    }
+  });
+  const documents = {};
+
+  io.on('connection', (socket) => {
+    console.log('connected');
+    let previousId;
+    const safeJoin = (currentId) => {
+      socket.leave(previousId);
+      socket.join(currentId);
+      previousId = currentId;
+    };
+  
+    socket.on("angular", (message) => {
+      console.log(message);
+    });
+  
+    socket.on("getDoc", docId => {
+      safeJoin(docId);
+      socket.emit("document", documents[docId]);
+    });
+  
+    socket.on("addDoc", doc => {
+      documents[doc.id] = doc;
+      safeJoin(doc.id);
+      io.emit("documents", Object.keys(documents));
+      socket.emit("document", doc);
+    });
+  
+    socket.on("editDoc", doc => {
+      documents[doc.id] = doc;
+      socket.to(doc.id).emit("document", doc);
+    });
+  
+    // io.emit("documents", Object.keys(documents));
+    socket.on('new-message', (message) => {
+      io.emit('new-message', message);
+    });
+  });
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -26,7 +71,7 @@ app.post('/webhooks',async(req,res)=>{
 })
 
 const port = process.env.PORT || 3000
-app.listen(port, ()=>{
+http.listen(port, ()=>{
     console.log('Server is running');
 });
 
