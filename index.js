4e81999906bdfb71a2d78713475decf796a90b3e
luisamorim <kicoamorim@gmail.com>
2020-12-17 15:40:33 -0300

    feat: add twilio

--- index.js
+++ index.js
@@ -8,70 +8,89 @@ const bodyParser = require('body-parser')
 
 const io = require('socket.io')(http, {
     cors: {
-      origin: '*',
+        origin: '*',
     }
-  });
-  const documents = {};
+});
+const documents = {};
 
-  io.on('connection', (socket) => {
+io.on('connection', (socket) => {
     console.log('connected');
     let previousId;
     const safeJoin = (currentId) => {
-      socket.leave(previousId);
-      socket.join(currentId);
-      previousId = currentId;
+        socket.leave(previousId);
+        socket.join(currentId);
+        previousId = currentId;
     };
-  
+
     socket.on("angular", (message) => {
-      console.log(message);
+        console.log(message);
     });
-  
+
     socket.on("getDoc", docId => {
-      safeJoin(docId);
-      socket.emit("document", documents[docId]);
+        safeJoin(docId);
+        socket.emit("document", documents[docId]);
     });
-  
+
     socket.on("addDoc", doc => {
-      documents[doc.id] = doc;
-      safeJoin(doc.id);
-      io.emit("documents", Object.keys(documents));
-      socket.emit("document", doc);
+        documents[doc.id] = doc;
+        safeJoin(doc.id);
+        io.emit("documents", Object.keys(documents));
+        socket.emit("document", doc);
     });
-  
+
     socket.on("editDoc", doc => {
-      documents[doc.id] = doc;
-      socket.to(doc.id).emit("document", doc);
+        documents[doc.id] = doc;
+        socket.to(doc.id).emit("document", doc);
     });
-  
+
     // io.emit("documents", Object.keys(documents));
     socket.on('new-message', (message) => {
-      io.emit('new-message', message);
+        io.emit('new-message', message);
     });
-  });
+});
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-async function bootstrap(){
+async function bootstrap() {
     mongo.connect();
 }
 
-app.get('/healthcheck', (req,res)=>{
+app.get('/healthcheck', (req, res) => {
     res.end('It\'s Alive!');
 });
 
-app.get('/webhooks',async(req,res)=>{
+app.get('/webhooks', async (req, res) => {
     const result = await mongo.getInstance().collection('webhooks').find({}).toArray();
     res.status(200).json(result);
 });
 
-app.post('/webhooks',async(req,res)=>{
+app.post('/webhooks', async (req, res) => {
     const result = await mongo.getInstance().collection('webhooks').insertOne(req.body);
+    socket.emit('twilio-message', req.body);
     res.status(201).json(result);
 })
 
+app.post('/sendMessage', async (req, res) => {
+    const to = req.body.To
+    const message = req.body.message
+    const accountSid = process.env.TWILIO_ACCOUNT_SID;
+    const authToken = process.env.TWILIO_AUTH_TOKEN;
+    const client = require('twilio')(accountSid, authToken);
+
+    const result = await client.messages
+        .create({
+            from: 'whatsapp:+14155238886',
+            body: message,
+            to: 'whatsapp:+' + to
+        });
+
+    res.json(result);
+
+});
+
 const port = process.env.PORT || 3000
-http.listen(port, ()=>{
+http.listen(port, () => {
     console.log('Server is running');
 });
 
